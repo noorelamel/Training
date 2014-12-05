919d76baec22f4ff557c2ba925e7e53514f317da
Dan <ddbeach87@gmail.com>
2014-12-05 16:00:08 -0700

    more swing

---
+++ PluralSight/Java/Mastering Java Swing - Part 1/Swing7 - Listeners/src/Toolbar.java
@@ -0,0 +1,52 @@
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+
+public class Toolbar extends JPanel implements ActionListener, StringListener {
+
+	private JButton helloButton;
+	private JButton goodbyeButton;
+	
+	private StringListener textListener;
+	
+	public Toolbar(){
+		helloButton = new JButton("Hello");
+		goodbyeButton = new JButton("Good-Bye");
+		
+		helloButton.addActionListener(this);
+		goodbyeButton.addActionListener(this);
+		
+		setLayout(new FlowLayout(FlowLayout.LEFT));
+		add(helloButton);
+		add(goodbyeButton);
+	}
+	
+	public void setStringListener(StringListener listener){
+		textListener = listener;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton clicked = (JButton) e.getSource();
+		if(clicked == helloButton){
+			if(textListener != null){
+				textListener.textEmitted("Hello");
+			}
+		}
+		else{
+			if(textListener != null){
+				textListener.textEmitted("Good-Bye");
+			}
+		}
+	}
+
+	@Override
+	public void textEmitted(String text) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ PluralSight/Java/Mastering Java Swing - Part 1/Swing8 - Size/src/FormPanel.java
@@ -0,0 +1,13 @@
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+
+public class FormPanel extends JPanel {
+	public FormPanel(){
+		Dimension dim = getPreferredSize();
+		System.out.println(dim);
+		dim.width = 250;
+		setPreferredSize(dim);
+	}
+}
