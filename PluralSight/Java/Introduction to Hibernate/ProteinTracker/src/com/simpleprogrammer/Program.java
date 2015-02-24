c11a02b64dd135ba4c7207cdfb3980dfd972a87f
Dan <ddbeach87@gmail.com>
2015-02-24 07:26:39 -0700

    Hibernate

--- PluralSight/Java/Introduction to Hibernate/ProteinTracker/src/com/simpleprogrammer/Program.java
+++ PluralSight/Java/Introduction to Hibernate/ProteinTracker/src/com/simpleprogrammer/Program.java
@@ -8,15 +8,33 @@ public class Program {
 		System.out.println("Hello Computer");
 
 		Session session = HibernateUtilities.getSessionFactory().openSession();
-		session.beginTransaction();
+//		session.beginTransaction();
 		
 		User user = new User();
-		user.setName("Joe");
-		user.setGoal(250);
-		session.save(user);
+//		user.setName("Joe");
+//		user.getProteinData().setGoal(250);
+//		session.save(user);
+		
+//		session.getTransaction().commit();
+		
+		session.beginTransaction();
+		
+		User loadedUser = (User) session.get(User.class, 1);
+		System.out.println(loadedUser.getId());
+		System.out.println(loadedUser.getName());
+		System.out.println(loadedUser.getProteinData().getGoal());
+		
+		// by doing this. when the commit is run below, it will update the database 
+		// with the new value for the loadedUser.
+
+		loadedUser.getProteinData().setTotal(loadedUser.getProteinData().getTotal() + 50);
+		loadedUser.getProteinData().setGoal(loadedUser.getProteinData().getGoal() + 50);
 		
 		session.getTransaction().commit();
+		
 		session.close();
+		
+		
 		HibernateUtilities.getSessionFactory().close();
 	}
 
---
+++ PluralSight/Java/Introduction to Hibernate/ProteinTracker/src/com/simpleprogrammer/ProteinData.java
@@ -0,0 +1,26 @@
+package com.simpleprogrammer;
+
+public class ProteinData {
+
+	private int total;
+	private int goal;
+	
+	public ProteinData() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	public int getGoal() {
+		return goal;
+	}
+	public void setGoal(int goal) {
+		this.goal = goal;
+	}
+	
+	
+}
